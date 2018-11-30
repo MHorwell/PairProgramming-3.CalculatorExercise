e2fe3d87be417b7147a16bcdccd8d13944f32a85
MHorwell <mattjhorwell@gmail.com>
2018-11-30 10:17:12 +0000

    Added functionality to add more arguments, no functionality for arguments greater than 3.

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,40 +1,44 @@
 package calculator;
 
 public class Calculator {
-	
+
 	public static void main(String[] args) {
-		System.out.println(calculator(15, 5 , 3));
-		
+		System.out.println(calculator(15, 5, 3));
+		System.out.println(calculator(3, 5, 15));
+
 	}
-	
+
 	public static String calculator(int numberOne, int numberTwo) {
 		if (numberOne == numberTwo) {
 			return (numberOne + " = " + numberTwo);
 		}
 		return "";
 	}
-	
-	
-	public static String calculator(int numberOne, int numberTwo, int numberThree) {
-		
-		if (calculator((numberOne / numberTwo), numberThree).equals((numberOne / numberTwo) + " = " + numberThree)) {
-				return (numberOne + " / " + numberTwo + " = " + numberThree);
-			}
-			
-		if	(calculator((numberOne * numberTwo), numberThree).equals((numberOne * numberTwo) + " = " + numberThree)) {
-				return (numberOne + " * " + numberTwo + " = " + numberThree);
-			}
-			
-		if	(calculator((numberOne + numberTwo), numberThree).equals((numberOne + numberTwo) + " = " + numberThree)) {
-				return (numberOne + " + " + numberTwo + " = " + numberThree);
-			}
-			
-		if	(calculator((numberOne - numberTwo), numberThree).equals((numberOne - numberTwo) + " = " + numberThree)) {
-				return (numberOne + " - " + numberTwo + " = " + numberThree);
-			}
-		
-			return "";
-		}
-	}
 
-	
+	public static String calculator(int... number) {
+
+		int count = number.length;
+
+		if (count == 3) {
+
+			if (calculator((number[0] / number[1]), number[2]).equals((number[0] / number[1]) + " = " + number[2])) {
+				return (number[0] + " / " + number[1] + " = " + number[2]);
+			}
+
+			if (calculator((number[0] * number[1]), number[2]).equals((number[0] * number[1]) + " = " + number[2])) {
+				return (number[0] + " * " + number[1] + " = " + number[2]);
+			}
+
+			if (calculator((number[0] + number[1]), number[2]).equals((number[0] + number[1]) + " = " + number[2])) {
+				return (number[0] + " + " + number[1] + " = " + number[2]);
+			}
+
+			if (calculator((number[0] - number[1]), number[2]).equals((number[0] - number[1]) + " = " + number[2])) {
+				return (number[0] + " - " + number[1] + " = " + number[2]);
+			}
+		
+		}
+
+		return "";
+	}
+}
