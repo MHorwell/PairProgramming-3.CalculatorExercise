ee6df445b8063df85f21a0679e9ae018cec463d2
Matthew Horwell <mattjhorwell@gmail.com>
2018-12-08 23:32:40 +0000

    Attempt made to implement functionality of multiple arguments, currently not working as intended.

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,44 +1,60 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
 
 	public static void main(String[] args) {
-		System.out.println(calculator(15, 5, 3));
-		System.out.println(calculator(3, 5, 15));
+		calculator(2, 1, 3);
 
 	}
 
-	public static String calculator(int numberOne, int numberTwo) {
-		if (numberOne == numberTwo) {
-			return (numberOne + " = " + numberTwo);
-		}
-		return "";
+	public static void calculator(int... number) {
+
+		String list = Integer.toString(number[0]);
+		calculator(list, number);
+
 	}
 
-	public static String calculator(int... number) {
+	public static void calculator(String list, int[] number) {
 
-		int count = number.length;
-
-		if (count == 3) {
-
-			if (calculator((number[0] / number[1]), number[2]).equals((number[0] / number[1]) + " = " + number[2])) {
-				return (number[0] + " / " + number[1] + " = " + number[2]);
+		if (number.length == 2) {
+			if (number[0] == number[1]) {
+				System.out.println(list + "=" + Integer.toString(number[1]));
+				//System.out.println(Integer.toString(number[0]) + "=" + Integer.toString(number[1]));
+			} else {
+				System.out.println(list + "!=" + Integer.toString(number[1]));
+				//System.out.println(Integer.toString(number[0]) + "!=" + Integer.toString(number[1]));
+				
 			}
-
-			if (calculator((number[0] * number[1]), number[2]).equals((number[0] * number[1]) + " = " + number[2])) {
-				return (number[0] + " * " + number[1] + " = " + number[2]);
-			}
-
-			if (calculator((number[0] + number[1]), number[2]).equals((number[0] + number[1]) + " = " + number[2])) {
-				return (number[0] + " + " + number[1] + " = " + number[2]);
-			}
-
-			if (calculator((number[0] - number[1]), number[2]).equals((number[0] - number[1]) + " = " + number[2])) {
-				return (number[0] + " - " + number[1] + " = " + number[2]);
-			}
-		
 		}
 
-		return "";
+		else {
+			
+			String multList = list + "*" + number[1];
+			int[] multNum = reducer((number[0] * number[1]), number);
+			calculator(multList, multNum);
+			
+			String addList = list + "+" + number[1];
+			int[] addNum = reducer((number[0] + number[1]), number);
+			calculator(addList, addNum);
+
+			if (number[1] != 0 && number[0] % number[1] == 0) {
+				String divList = list + "/" + number[1];
+				int[] divNum = reducer(number[0] / number[1], number);
+				calculator(divList, divNum);
+			}
+			
+			String subList = list + "-" + number[1];
+			int[] subNum = reducer(number[0] - number[1], number);
+			calculator(subList, subNum);
+		}
+
 	}
+
+	public static int[] reducer(int i, int[] array) {
+		array[1] = i;
+		return Arrays.copyOfRange(array, 1, array.length);
+	}
+
 }
